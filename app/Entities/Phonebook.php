c25df355d29a7d23ce9600e7ea82929726854eeb
German Bianchini <bianchiniger@gmail.com>
2020-02-11 01:13:45 -0300

    Refactor on the ORMish initial idea on the Phonebook entity

--- app/Entities/Phonebook.php
+++ app/Entities/Phonebook.php
@@ -5,34 +5,6 @@ include_once('Entity.php');
 class Phonebook extends Entity {
     private $table = 'phonebooks';
 
-    private $id;
-    private $name;
-    private $description;
-
-    public function getId() {
-        return $this->id;
-    }
-
-    public function setId($id) {
-        $this->id = $id;
-    }
-
-    public function getName() {
-        return $this->name;
-    }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function getDescription() {
-        return $this->description;
-    }
-
-    public function setDescription($description) {
-        $this->description = $description;
-    }
-
     public function create($data) {
         $query = "INSERT INTO $this->table SET name=:name, description=:description";
         $stmt = $this->getDBResource()->prepare($query);
